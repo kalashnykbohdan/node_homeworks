f6db2e9234de1ae4db5e9c5e0ed785c675626868
Kalashnyk Bohdan <space.bohdan@gmail.com>
2020-09-15 22:00:41 +0300

    first commit homework1

---
+++ homework1/homework1/index.js
@@ -0,0 +1,35 @@
+// index.js
+
+const contacts = require('./contacts.js');
+
+const argv = require('yargs').argv;
+console.log(argv);
+
+
+
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+    console.log('invokeAction');
+  switch (action) {
+    case 'list':
+      contacts.listContacts();
+      break;
+
+    case 'get':
+      contacts.getContact(id);
+      break;
+
+    case 'add':
+      contacts.addContact(name, email, phone);
+      break;
+
+    case 'remove':
+      contacts.removeContact(id);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
